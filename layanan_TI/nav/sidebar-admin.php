9b376051823e6dbb3ff8e4f905ae9a057e2481cd
Zikra21-MD <zikramulya2112@gmail.com>
2024-12-11 21:19:59 +0700

    Add files via upload

---
+++ layanan_TI/nav/sidebar-admin.php
@@ -0,0 +1,8 @@
+<div class="container">
+        <div class="sidebar">
+            <ul>
+                <li><a href="../admin/main.php">Dashboard</a></li>
+                <li><a href="../admin/user_manage.php" class="active">Kelola Pengguna</a></li>
+                <li><a href="../admin/masalah.php">Kelola Masalah</a></li>
+            </ul>
+        </div>
