73f2900c7444ed2655ecc415848859c2f68cf725
Zikra21-MD <zikramulya2112@gmail.com>
2024-12-11 21:23:28 +0700

    Delete layanan_TI directory

--- layanan_TI/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-header("Location: sign_in.php");
-exit;
-?>
--- layanan_TI/nav/sidebar-admin.php
+++
@@ -1,8 +0,0 @@
-<div class="container">
-        <div class="sidebar">
-            <ul>
-                <li><a href="../admin/main.php">Dashboard</a></li>
-                <li><a href="../admin/user_manage.php" class="active">Kelola Pengguna</a></li>
-                <li><a href="../admin/masalah.php">Kelola Masalah</a></li>
-            </ul>
-        </div>
